c99abf5e5d9a7afe059721f3feb33d58d6f95515
Rishabh Bhatnagar <rishabh.bhatnagar@delta.com>
2026-04-02 09:19:09 +0530

    Be specific with the tool names
    Signed-off-by: Rishabh Bhatnagar <rishabh.bhatnagar@delta.com>

--- src/models/tool.py
+++ src/models/tool.py
@@ -5,5 +5,5 @@ class ToolNames(str, enum.Enum):
     RESOURCE_FETCHER = "resource_fetcher"
     RESOURCE_FETCHER_BY_STACK_NAME = "resource_fetcher_by_stack_name"
     RESOURCE_FETCHER_BY_BLOCK_CODE = "resource_fetcher_by_block_code"
-    GENERATE_AUDIT_REPORT = "generate_audit_report"
+    GENERATE_AUDIT_REPORT = "generate_audit_report_by_block_code"
     GENERATE_AUDIT_REPORT_BY_STACK_NAME = "generate_audit_report_by_stack_name"
